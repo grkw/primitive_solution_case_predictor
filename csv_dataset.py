1800b508c7a370aaaeab404453c2451817a6222c
Grace0 <gracekwak25@gmail.com>
2024-08-18 17:38:23 -0700

    Add extracting training examples from csv

--- csv_dataset.py
+++ csv_dataset.py
@@ -4,6 +4,60 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
+class ConvertCSVToExamples():
+    def __init__(self, csv_fname):
+        self.data = pd.read_csv(csv_fname)
+        print("Original data shape: ", self.data.shape)
+        # Delete rows with invalid input
+        self.data = self.data[self.data['ruckig_solution_x'] != 'INVALID_INPUT']
+        self.data = self.data.reset_index(drop=True)
+
+        # Replace _UDDU or _UDUD with empty string
+        self.data['ruckig_solution_x'] = self.data['ruckig_solution_x'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_y'] = self.data['ruckig_solution_y'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_z'] = self.data['ruckig_solution_z'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_x'] = self.data['ruckig_solution_x'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_y'] = self.data['ruckig_solution_y'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_z'] = self.data['ruckig_solution_z'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_x'] = self.data['ruckig_solution_x'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_y'] = self.data['ruckig_solution_y'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_z'] = self.data['ruckig_solution_z'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_x'] = self.data['ruckig_solution_x'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_y'] = self.data['ruckig_solution_y'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_z'] = self.data['ruckig_solution_z'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_x'] = self.data['ruckig_solution_x'].replace('_UDUD|_UDDU', '', regex=True)
+        self.data['ruckig_solution_y'] = self.data['ruckig_solution_y'].replace('_UDUD|_UDDU', '', regex=True)
+
+        self.indep_min_x_loc = int(self.data.columns.get_loc('indep_min_x'))
+
+        print("Data shape after removing invalids: ", self.data.shape)
+
+    def generateExamplesCSV(self, output_fname):
+
+        dataset = pd.DataFrame(columns=['pi', 'vi', 'ai', 'pf', 'vf', 'af', 't_profile', 'ruckig_solution'])
+        print("Generating examples")
+        for idx in range(self.data.shape[0]): # Iterate over all rows
+            print(idx)
+            limiting_dof = int(self.data.at[idx, 'limiting_dof'])
+            t_profile = self.data.iat[idx, self.indep_min_x_loc+limiting_dof]
+            
+            for dof_idx in range(3): # 0, 1, 2
+                
+                if (limiting_dof == dof_idx):
+                    continue
+
+                pi = self.data.iat[idx, 0+dof_idx]
+                vi = self.data.iat[idx, 3+dof_idx]
+                ai = self.data.iat[idx, 6+dof_idx]
+                pf = self.data.iat[idx, 9+dof_idx]
+                vf = self.data.iat[idx, 12+dof_idx]
+                af = self.data.iat[idx, 15+dof_idx]
+                ruckig_solution = self.data.iat[idx, 18+dof_idx]
+                
+                new_row = pd.DataFrame({'pi': [pi], 'vi': [vi], 'ai': [ai], 'pf': [pf], 'vf': [vf], 'af': [af], 't_profile': [t_profile], 'ruckig_solution': [ruckig_solution]})
+                dataset = pd.concat([dataset, new_row], ignore_index=True)
+        dataset.to_csv(output_fname, index=False)
+
 class CSVDataset(Dataset):
     def __init__(self, csv_files, input_col, label_cols, num_sample, transform=None):
 
@@ -65,3 +119,8 @@ class CSVDataset(Dataset):
         #     sample = self.transform(sample)
 
         return inputs, outputs
+
+if __name__ == "__main__":
+    csv_fname = 'data/train_1scale_7seed_5max_5amax.csv'
+    examples = ConvertCSVToExamples(csv_fname)
+    examples.generateExamplesCSV('data/train_1scale_7seed_5max_5amax_examples.csv')
